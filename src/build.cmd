e4a490ff2c8533894f8235e09e6732121758fdc6
Sean Kearney <git@seankearney.com>
2014-08-01 15:07:53 -0400

    assume nuget is in PATH

--- src/build.cmd
+++ src/build.cmd
@@ -1,5 +1,5 @@
 @echo off
 cls
-"..\lib\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
+"NuGet.exe" "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
 "packages\FAKE\tools\Fake.exe" build.fsx
 pause
